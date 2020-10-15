585db5cfe345a0af170a65309aad8b4e306d71cc
Benício Barreto <benicio.daniel@gmail.com>
2020-10-15 18:49:32 -0300

    Added react navigation. Created routes

---
+++ src/routes.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const AppStack = createStackNavigator();
+const { Navigator, Screen } = AppStack;
+
+import OrphanagesMap from "./pages/OrphanagesMap";
+import OrphanageDetails from "./pages/OrphanageDetails";
+
+export default function Routes() {
+  return (
+    <NavigationContainer>
+      <Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Screen name="OrphanagesMap" component={OrphanagesMap} />
+        <Screen name="OrphanageDetails" component={OrphanageDetails} />
+      </Navigator>
+    </NavigationContainer>
+  );
+}
